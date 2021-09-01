f552a41f19d08c4d47fa05f309a30495cf6b1864
Dimple1243 <89607801+Dimple1243@users.noreply.github.com>
2021-09-01 07:17:53 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,26 @@
+num1=int(input("enter the first_number"))
+num2=int(input("enter the second_number"))
+symbol=(input("enter your symbol"))
+if symbol=="+":
+    print(num1+num2)
+elif symbol=="-":
+    print(num1-num2)
+elif symbol=="*":
+    print(num1*num2)   
+elif symbol=="/":
+    print(num1/num2)
+elif symbol=="//":
+    print(num1//num2)  
+elif symbol=="**":
+    print(num1**num2)  
+else:
+    print("check your value")
+
+
+    
+
+
+
+
+
+
